9816965ca83a94673dfc4f6389ec00dcea4c2d76
Angelo Pirola <angelo9871@gmail.com>
2025-04-09 08:50:15 +0200

    feat: Initialize Docusaurus site with essential configuration files
    - Added globalData.json for documentation structure and sidebar configuration.
    - Created i18n.json for internationalization settings.
    - Introduced registry.js to manage component imports and chunking.
    - Established routes.js to define application routing and navigation.
    - Generated routesChunkNames.json for mapping routes to chunk names.
    - Added site-metadata.json to store site and plugin version information.
    - Created site-storage.json for local storage configuration.

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("E:\\Disco_E\\Progetti\\GSWCloudApp.Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("E:\\Disco_E\\Progetti\\GSWCloudApp.Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("E:\\Disco_E\\Progetti\\GSWCloudApp.Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("E:\\Disco_E\\Progetti\\GSWCloudApp.Docs\\src\\css\\custom.css"),
+];
